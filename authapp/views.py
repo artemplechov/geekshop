5cea54911e2fde993fe31d270f973fef8c5dbb62
artemplechov <artemplechov@gmail.com>
2021-11-26 22:18:02 +0300

    lesson_4 задания с 1 по 4

--- authapp/views.py
+++ authapp/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from authapp.forms import UserLoginForm,UserRegisterForm
+from authapp.forms import UserLoginForm, UserRegisterForm
+
 
 # Create your views here.
 def login(request):
@@ -12,9 +13,9 @@ def login(request):
         if form.is_valid():
             username = request.POST.get('username')
             password = request.POST.get('password')
-            user = auth.authenticate(username=username,password=password)
+            user = auth.authenticate(username=username, password=password)
             if user.is_active:
-                auth.login(request,user)
+                auth.login(request, user)
                 return HttpResponseRedirect(reverse('index'))
         else:
             print(form.errors)
@@ -22,13 +23,29 @@ def login(request):
         form = UserLoginForm()
     context = {
         'title': 'Geekshop | Авторизация',
-        'form' : form
+        'form': form
     }
-    return render(request,'authapp/login.html',context)
+    return render(request, 'authapp/login.html', context)
 
 
 def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('authapp:login'))
+        else:
+            print(form.errors)
+    else:
+        form = UserRegisterForm()
+
     context = {
         'title': 'Geekshop | Регистрация',
+        'form': form
     }
-    return render(request,'authapp/register.html',context)
+    return render(request, 'authapp/register.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return render(request,'mainapp/index.html')
