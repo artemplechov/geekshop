043189106e83a1379eb409b5d71e80fb3e390bdb
artemplechov <artemplechov@gmail.com>
2021-12-05 23:11:12 +0300

    lesson_7 detail.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 import json
 import os
+
+from django.views.generic import DetailView
+
 from mainapp.models import ProductCategory, Product
 
 MODULE_DIR = os.path.dirname(__file__)
@@ -51,3 +54,14 @@ def products(request):
     context['products'] = Product.objects.all()
     return render(request, 'mainapp/products.html', context)
 
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'mainapp/detail.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetail,self).get_context_data(**kwargs)
+        product = self.get_object()
+        context['product'] = product
+        return context
