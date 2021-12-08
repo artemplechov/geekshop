f95c55b16baeab4afee1a4f02828b5bbacd4dc39
artemplechov <artemplechov@gmail.com>
2021-12-08 23:42:58 +0300

    lesson_8 фильтрация

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,5 +4,6 @@ from mainapp.views import products, ProductDetail
 app_name='products'
 urlpatterns = [
     path('', products,name='products'),
-    path('detail/<int:pk>/', ProductDetail.as_view(),name='detail')
+    path('detail/<int:pk>/', ProductDetail.as_view(),name='detail'),
+    path('category/<int:id_category>/', products,name='category')
 ]
